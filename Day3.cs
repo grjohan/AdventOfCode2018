8bb5359050822f702ea5eb004b0ac3ff81cf026a
grjohan <johan.gronberg@gmail.com>
2018-12-02 15:15:05 +0900

    Add day3 file for a headstart

---
+++ Day3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Day3 : Day, DailySolution<int, int>
+    {
+        public int Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
